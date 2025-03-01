de36cbb2017f2c2266b1f27a3d0dfd3fa3a86eaf
piyush0528 <tiwaripiyush262@gmail.com>
2025-03-01 14:43:01 +0530

    added : get request

---
+++ src/main/java/com/example/FirstSpringApp/User.java
@@ -0,0 +1,22 @@
+package com.example.FirstSpringApp;
+
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
